aa852d00df11467ee536ae9fe2c0ad552ef296d2
zouning68 <zouning9168@163.com>
2020-06-06 16:16:48 +0800

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-SCRIPT_NAME=run_estimator.py
+SCRIPT_NAME=server.py
 
 echo "run file: " $SCRIPT_NAME
 LOG_NAME=${SCRIPT_NAME}".log"
 
 ps -ef | grep $SCRIPT_NAME | grep -v grep | awk '{print $2}' | xargs kill -9
 
-nohup python $SCRIPT_NAME > $LOG_NAME 2>&1&
-tail -f $LOG_NAME
+nohup python $SCRIPT_NAME &
+
+#nohup python $SCRIPT_NAME > $LOG_NAME 2>&1&
+#tail -f $LOG_NAME
